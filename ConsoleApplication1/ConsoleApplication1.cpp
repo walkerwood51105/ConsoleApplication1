8b90da3966cdface66fa223088835081e3c24451
Walker Wood <wwood18@email.tjc.edu>
2025-08-28 20:07:35 -0500

    oh, here we go buddy.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,7 +8,7 @@ int main()
     std::cout << "Hello World!\n";
 
     //asdfasdf
-}
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
